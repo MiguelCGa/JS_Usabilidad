3feae0a55ca62edf17f080c89169799d8b8926ef
MiguelCGa <micurros@ucm.es>
2024-10-31 15:11:16 +0100

    Sobraba

--- My project/Assets/Scripts/Interactions/InteractableItem.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class InteractableItem : IInteractable {
-    public override void OnClick() {
-        Debug.Log("Interactua con el objeto");
-    }
-}
