a4c6adcce9c69c4ef4fd4c35e282433d7f02530e
DDiegoP <95857593+DDiegoP@users.noreply.github.com>
2024-11-07 15:15:53 +0100

    Dialogue and Response structure

---
+++ My project/Assets/Scripts/ConversationManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ConversationManager : MonoBehaviour
+{
+    private DialogueData data;
+
+    [SerializeField]
+    private DialogueBox dialogue;
+
+
+}
--- My project/Assets/Scripts/Dialogues/DialogueData.cs
+++ My project/Assets/Scripts/Dialogues/DialogueData.cs
@@ -1,10 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DialogueData : MonoBehaviour
 {
-    string jsonFile = "dialogues";
+    [SerializeField]
+    private TextAsset dialoguesJsonFile;
+    [SerializeField]
+    private TextAsset responsesJsonFile;
 
+    private DialogueGroup[] myDialogueGroup;
+    private ResponseGroup[] myResponseGroup;
 
+    void Start()
+    {
+        myDialogueGroup = JsonUtility.FromJson<DialogueGroup[]>(dialoguesJsonFile.text);
+        myResponseGroup = JsonUtility.FromJson<ResponseGroup[]>(responsesJsonFile.text);
+    }
+
+    public DialogueGroup GetDialogueGroupByID(string id)
+    {
+        DialogueGroup dialogueGroup = null;
+        for(int i = 0; i < myDialogueGroup.Length; i++)
+        {
+            if(id == myDialogueGroup[i].id)
+            {
+                dialogueGroup = myDialogueGroup[i];
+            }
+        }
+        return dialogueGroup;
+    }
+    public ResponseGroup GetResponseGroupByID(string id)
+    {
+        ResponseGroup responseGroup = null;
+        for(int i = 0; i < myResponseGroup.Length; i++)
+        {
+            if(id == myResponseGroup[i].id)
+            {
+                responseGroup = myResponseGroup[i];
+            }
+        }
+        return responseGroup;
+    }
+}
+
+[System.Serializable]
+public class DialogueGroup
+{
+    public string id;
+    public Dialogue[] dialogues;
+}
+[System.Serializable]
+public class Dialogue
+{
+    public string text;
+    public string nextResponseGroup;
+}
+[System.Serializable]
+public class ResponseGroup
+{
+    public string id;
+    public Response[] responses;
+}
+[System.Serializable]
+public class Response
+{
+    public string text;
+    public string nextDialogueGroup;
+    public bool chosen;
 }
