63e5fe4efada44e1db25ea25a383e9eb5dbafc92
Jjianuo <jwen@ucm.es>
2025-05-15 12:29:30 +0200

    Recorridos grafos de dialogo
    Co-Authored-By: Rocio Sanchez <82374311+Bimbloc@users.noreply.github.com>

---
+++ My Project/Assets/Scripts/BotJSONParser.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Defective.JSON;
+
+public class BotJSONParser 
+{
+    public Dictionary<string, List<Tuple<string, int>>> responseDictionary { get; private set; }
+    public Dictionary<string, string> dialogueDictionary { get; private set; }
+
+    public void LoadDialogueJson(string level)
+    {
+        dialogueDictionary = new Dictionary<string, string>(); ;
+
+        using (StreamReader r = new StreamReader(level))
+        {
+            string json = r.ReadToEnd();
+
+            JSONObject levelJsonObject = new JSONObject(json);
+            JSONObject dialogueJsonObject = levelJsonObject.list[0];
+            string lastResponse;
+            for (int i = 0; i < dialogueJsonObject.list.Count; ++i)
+            {
+                JSONObject dialogues = dialogueJsonObject.list[i];
+                lastResponse = dialogues.list[dialogues.list.Count - 1].GetField("Responses").stringValue;
+                dialogueDictionary.Add(dialogueJsonObject.keys[i], lastResponse);
+            }
+        }
+    }
+
+    public void LoadResponsesJson(string level)
+    {
+        responseDictionary = new Dictionary<string, List<Tuple<string, int>>>();
+
+        using (StreamReader r = new StreamReader(level))
+        {
+            string json = r.ReadToEnd();
+
+            JSONObject levelJsonObject = new JSONObject(json);
+            JSONObject responseJsonObject = levelJsonObject.list[0];
+
+            for (int i = 0; i < responseJsonObject.list.Count; ++i)
+            {
+                responseDictionary.Add(responseJsonObject.keys[i], new List<Tuple<string, int>>());
+                for (int j = 0; j < responseJsonObject.list[i].count; ++j)
+                {
+                    JSONObject dialogues = responseJsonObject.list[i];
+                    string nextDialogueGroup = dialogues.list[j].GetField("nextDialogueGroup").stringValue;
+                    int tension = dialogues.list[j].GetField("tension").intValue;
+                    responseDictionary[responseJsonObject.keys[i]].Add(new Tuple<string, int>(nextDialogueGroup, tension));
+                }
+            }
+        }
+    }
+}
